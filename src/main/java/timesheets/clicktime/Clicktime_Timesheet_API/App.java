03402fd085a03cb866ae7a08cc41f1bbe50073a9
Anand Shukla <anands@xpanxion.co.in>
2018-06-03 03:21:46 +0530

    Base project

---
+++ src/main/java/timesheets/clicktime/Clicktime_Timesheet_API/App.java
@@ -0,0 +1,13 @@
+package timesheets.clicktime.Clicktime_Timesheet_API;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
